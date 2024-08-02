821f3d0ee8210215941100516a9d5b453da8ffff
Deveshyadav07 <deveshyadav55655@gmail.com>
2024-08-02 12:59:29 +0530

    learn about JavaScript Variables, Data Types & Objects |

---
+++ JAVASCRIPT/day 01/script.js
@@ -0,0 +1,43 @@
+console.log("this is my first java code");
+var a = 10;
+var b = 20;
+var c = a + b;
+console.log(c);
+console.log(a + b + 50);
+var d = "DEVESH BHAI";
+var e = true;
+//type of = find datatype 
+console.log(typeof a, typeof b, typeof c, typeof d, typeof e);
+{
+    let a = 102;
+    console.log(a);
+    a = a + 8;
+    console.log(a);
+}
+console.log(a);
+const A = 5;
+//A  = A  +4;    //  Not Allowed because A is constant
+
+let x = "Devesh yadav";
+let y = 7;
+let z = 7.86;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    name: "devesh",
+    "Roll number": 7,
+    "pin code": 222148,
+    is_handsome: true
+
+}
+console.log(o);
+o.name = "devesh yadav";
+console.log(o);
+o["pin code"] = 222149;
+console.log(o);
+
